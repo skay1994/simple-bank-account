5b5914c8b2ef782e38775ccc5b1c1f2cd2b1f749
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-08 00:01:55 -0300

    Feat: Create save_file and open_file functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::File;
+use std::io::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    save_file("{\"some\": \"json\"}".to_string());
+    println!("Hello, world! {}", open_file());
+}
+
+fn open_file() -> String {
+    let mut content = String::new();
+    let mut file = File::open("accounts.json")
+        .expect("Err: File accounts.json not found");
+
+    file.read_to_string(&mut content)
+        .expect("Err: Failed to read \"accounts.json\" content.");
+
+    content
+}
+
+fn save_file(content: String) {
+    let mut file = File::create("accounts.json")
+        .expect("Err: Error to create \"accounts.json\" file.");
+
+    file.write_all(content.as_bytes())
+        .expect("Err: Failed to save a file content:");
 }
