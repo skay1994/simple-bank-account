2a5b6cdd9967482f225326f6e813555fb5adccc9
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-13 16:08:06 -0300

    Feat: Create full_name method

--- src/structs/user_struct.rs
+++ src/structs/user_struct.rs
@@ -3,3 +3,9 @@ pub struct User {
     pub name: String,
     pub last_name: String,
 }
+
+impl User {
+    pub fn full_name(&self) -> String {
+        format!("{} {}", self.name, self.last_name)
+    }
+}
