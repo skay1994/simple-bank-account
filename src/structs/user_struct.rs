9d27a7d8dd66a5bbf04f6d8c3d051204d9cef160
Jorge Carlos <skay_1994@yahoo.com.br>
2023-01-13 16:08:26 -0300

    Feat: Create tests for User struct

--- src/structs/user_struct.rs
+++ src/structs/user_struct.rs
@@ -9,3 +9,18 @@ impl User {
         format!("{} {}", self.name, self.last_name)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_user_create() {
+        let user = User {
+            name: String::from("Jorge"),
+            last_name: String::from("Carlos")
+        };
+
+        assert_eq!(user.full_name(), "Jorge Carlos")
+    }
+}
